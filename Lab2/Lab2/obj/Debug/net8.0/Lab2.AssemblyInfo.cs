1dca8f0034961bea334bc1483de1762ff6afecc8
trintt22-ux <trintt22@uef.edu.vn>
2025-12-01 11:46:55 +0700

    init

--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e629966da43f809319b71d7a9ffd1356178d6aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e2174dc3a32f4c64780a3255f1ab4134a5c020")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
